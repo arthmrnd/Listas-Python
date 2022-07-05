5ccbb7782c9843abf0a8fca52c9e5558179a0c48
Arthur Miranda <theodinksson@gmail.com>
2022-07-05 19:09:36 -0300

    Create c.py

---
+++ Lista 2/c.py
@@ -0,0 +1,14 @@
+l1, p1 = input().split()
+l2, p2 = input().split()
+
+l1 = int(l1)
+l2 = int(l2)
+
+p1 = int(p1)
+p2 = int(p2)
+
+
+if (l1==l2 and p1==p2):
+    print("Soltar pacote")
+else:
+    print("Nao soltar pacote")
