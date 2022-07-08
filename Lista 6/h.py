496f21b1013af53d2a8f1c9a5427cd0359423dd9
Arthur Miranda <theodinksson@gmail.com>
2022-07-08 16:29:01 -0300

    Create h.py

---
+++ Lista 6/h.py
@@ -0,0 +1,44 @@
+def binary(array, x):
+    l = 0
+    r = len(array) - 1
+    
+    while (l <= r): 
+        m = (l + r) // 2 
+        if (array[m] == x):
+            l = r +1
+        elif (array[m] < x): 
+            l = m + 1
+        else: 
+            r = m - 1
+            
+    cnt = 0
+    while(cont == 0):
+        if(m <= r and array[m] <= x):
+            m += 1
+        else:
+            cnt = 1
+    return m
+
+n, m = input().split()
+n = int(n)
+m = int(m)
+
+mat = []
+cont = 0
+while(cont < n):
+    li = input().split()
+    for xap in li:
+        mat.append(int(xap))
+    cont += 1
+
+mat.sort()
+
+c = int(input())
+con = input().split()
+con = list(map(int, con))
+cont = 0
+while(cont < c):
+    sum = 0
+    las = binary(mat,con[cont])
+    print(las)
+    cont += 1
